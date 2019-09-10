335a7f734f56000a7f640c7dc580bfb31fac5d43
notDivan <kanishka.diva@gmail.com>
2019-09-10 11:07:40 +0530

    update

--- Navigations/Mealsnavigator.js
+++ Navigations/Mealsnavigator.js
@@ -1,10 +1,19 @@
+import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
+import {
+  createBottomTabNavigator,
+  createTabNavigator
+} from "react-navigation-tabs";
+
 import { createAppContainer } from "react-navigation";
 import CatergoriesScreen from "../screens/CatergoriesScreen";
 import CategoryMealScreen from "../screens/CategoryMealScreen";
 import MealDetails from "../screens/MealDetail";
 import Colours from "../constants/Colours";
+import MealsItems from "../components/mealsItems";
+import FavoritesScreen from "../screens/FavoriteScreen";
 
+import { Ionicons } from "@expo/vector-icons";
 const MealNavigator = createStackNavigator(
   {
     Categories: CatergoriesScreen,
@@ -24,4 +33,39 @@ const MealNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealNavigator);
+const MealsFavNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealNavigator,
+      navigationOptions: {
+        tabBarIcon: tabInfo => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        }
+      }
+    },
+
+    Favorite: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarIcon: tabInfo => {
+          return (
+            <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+          );
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colours.accent
+    }
+  }
+);
+
+export default createAppContainer(MealsFavNavigator);
